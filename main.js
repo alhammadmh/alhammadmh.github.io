aa219a2db8eb3338987061a80d7077489ee57d2d
mohammed <alhammad2013@gmail.com>
2019-03-12 16:16:06 +0300

    first draft

---
+++ main.js
@@ -0,0 +1,31 @@
+
+
+var player_One='x';
+
+
+// function nextPlayer(cell){
+//   cell.innerText= player_One;
+//   switchPlayer();
+//   }
+
+
+$(".cell").on("click", function turn (event) {
+  if(checkWinner(player_One)){
+    alert('Congrats '+player_One+' Player Win');
+  }
+  else if(player_One == 'x') {$(event.target).css("background-image", "url(x_img.png)").css('background-size','160px').css('background-color', '#7ea0d6');
+   player_One = 'o';
+} else {  $(event.target).css("background-image", "url(o_img.png)").css('background-size','160px').css('background-color', '#bfb8b7')
+  player_One = 'x';
+}})
+
+
+
+
+// Reset button function 
+$('button').on('click',function(){
+  $('.cell').css('background','grey')
+})
+
+
+  
